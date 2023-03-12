ecf429bb3a26542f8a91d0b8f30379e0c14afd13
Bokinha <pedrohenrique_maciel@hotmail.com>
2023-03-12 16:20:11 -0300

    Second Commit

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,9 +1,23 @@
+import Banner from "componentes/Banner";
+import Card from "componentes/Card";
+import Titulo from "componentes/Titulo";
+import videos from 'json/db.json';
+import styles from './Inicio.module.css';
 
-
-function Inicio () {
+function Inicio() {
     return (
-        <h1>Olá mundo! Estou numa página nova uhul!</h1>
+        <>
+            <Banner imagem="home" />
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
+        </>
     )
 }
 
-export default Inicio
+export default Inicio;
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,24 @@
-import Inicio from "./pages/Inicio"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Favoritos from "pages/Favoritos";
+import Inicio from "pages/Inicio";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Player from './pages/Player/index';
+import NaoEncontrada from './pages/NaoEncontrada/index';
+import PaginaBase from './pages/PaginaBase/index';
 
-function AppRoutes() {
+function AppRoutes ()
+{
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Inicio />}></Route>
+                <Route path="/" element={ <PaginaBase /> }>
+                    <Route index element={ <Inicio /> }></Route>
+                    <Route path="favoritos" element={ <Favoritos /> }></Route>
+                    <Route path=":id" element={ <Player /> }></Route>
+                    <Route path="*" element={ <NaoEncontrada /> }></Route>
+                </Route>
             </Routes>
         </BrowserRouter>
     )
 }
 
-export default AppRoutes
+export default AppRoutes;
